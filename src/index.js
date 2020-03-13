0aaa3b01990ad755d7813e897c6130d0a83caac7
junpay <junpoko625@gmail.com>
2020-03-13 15:02:06 +0900

    fix typo from

--- src/index.js
+++ src/index.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import { createStore } form 'redux';
+import { createStore } from 'redux';
 import { Provider } from 'react-redux';
 
 import './index.css';
