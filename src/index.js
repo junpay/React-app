20b8896c694e27b675692513e91171135a9315a8
junpay <junpoko625@gmail.com>
2020-03-31 11:34:04 +0900

    implement event new component

--- src/index.js
+++ src/index.js
@@ -3,10 +3,12 @@ import ReactDOM from 'react-dom';
 import { createStore, applyMiddleware } from 'redux';
 import { Provider } from 'react-redux';
 import thunk from 'redux-thunk';
+import { BrowserRouter, Route, Switch } from 'react-router-dom'
 
 import './index.css';
 import reducer from './reducers';
 import EventsIndex from './components/events_index';
+import EventsNew from './components/events_new';
 import * as serviceWorker from './serviceWorker';
 
 const store = createStore(reducer, applyMiddleware(thunk))
@@ -14,7 +16,12 @@ const store = createStore(reducer, applyMiddleware(thunk))
 ReactDOM.render(
   // Storeをどのcomponentからも利用できるように既存のcomponentをProviderコンポーネントでwrapし、store属性に作成したstoreを指定する。
   <Provider store={store}>
-    <EventsIndex />
+    <BrowserRouter>
+      <Switch>
+        <Route exact path="/events/new" component={EventsNew} />
+        <Route exact path="/" component={EventsIndex} />
+      </Switch>
+    </BrowserRouter>
   </Provider>,
   document.getElementById('root')
 );
--- src/reducers/index.js
+++ src/reducers/index.js
@@ -1,9 +1,10 @@
 // 全Reducerを一つのReducerに結合する。
 // combineReducersがReducerを結合するメソッド
 import { combineReducers } from 'redux';
+import { reducer as form } from 'redux-form'
 import events from './events'
 
-export default combineReducers({ events })
+export default combineReducers({ events, form })
 
 // 複数のReducerを結合する場合
 // export default combineReducers({ foo, bar, baz })
