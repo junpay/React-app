289cc1d3143ca59083492b405ba5b47ad9ebdcc3
junpay <junpoko625@gmail.com>
2020-03-31 10:35:55 +0900

    implement events index

--- src/actions/index.js
+++ src/actions/index.js
@@ -1,11 +1,15 @@
-export const INCREMENT = 'INCREMENT'
-export const DECREMENT = 'DECREMENT'
+import axios from 'axios';
+
+export const READ_EVENTS = 'READ_EVENTS'
+
+const ROOT_URL = 'https://udemy-utils.herokuapp.com/api/v1'
+const QUERYSTRING = '?token=token123'
 
 // Actionを返す関数（アクションクリエイター）を作成
-export const increment = () => ({
-  type: INCREMENT
-})
-
-export const decrement = () => ({
-  type: DECREMENT
-})
+// 純粋なオブジェクトを返さなければならない。
+// redux thunkを使えばfunctionで返せる
+export const readEvents = () => async dispatch => {
+  const response = await axios.get(`${ROOT_URL}/events${QUERYSTRING}`)
+  // console.log(response)
+  dispatch({type: READ_EVENTS, response})
+}
--- src/reducers/index.js
+++ src/reducers/index.js
@@ -1,9 +1,9 @@
 // 全Reducerを一つのReducerに結合する。
 // combineReducersがReducerを結合するメソッド
 import { combineReducers } from 'redux';
-import count from './count'
+import events from './events'
 
-export default combineReducers({ count })
+export default combineReducers({ events })
 
 // 複数のReducerを結合する場合
 // export default combineReducers({ foo, bar, baz })
